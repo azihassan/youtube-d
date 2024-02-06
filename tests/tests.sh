a24b7b7daaf89f86162597e60b5b019575847d1a
Azi Hassan <azi.hassan@live.fr>
2024-02-06 21:30:30 +0100

    [feature/ISSUE-56] Enable implicit dethrottling

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,7 +1,7 @@
 set -e
 chmod +x youtube-d
 
-./youtube-d -p -d --no-progress https://www.youtube.com/watch?v=R85MK830mMo
+./youtube-d -p --no-progress https://www.youtube.com/watch?v=R85MK830mMo
 
 filename="Debugging Github actions-R85MK830mMo-18.mp4"
 if [ ! -e "$filename" ]; then
